50297a654a1ed680ab922b46e11a6f89dbda03fe
miphung <michellephung323@gmail.com>
2016-10-17 11:28:50 -0700

    corrected everything

--- NamedCow.java
+++ NamedCow.java
@@ -1,9 +1,11 @@
 class NamedCow extends Cow
 {
-	protected String myName;
-	public NamedCow(String name)
+	private String myName;
+	public NamedCow(String type, String name, String sound)
 	{
 		myName = name;
+		myType = type;
+     	mySound = sound;
 	}
 	public NamedCow()
 	{
